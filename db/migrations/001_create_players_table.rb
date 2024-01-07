100134e9218e63b81ee054146da3634ec7f11008
Dev Miduz <devmiduz@gmail.com>
2024-01-07 13:11:42 +0000

    Added players table migration.

---
+++ db/migrations/001_create_players_table.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+# 001_create_players_table.rb
+
+# Create a players table with:
+#    - id (primary_key)
+#    - key (String 128)
+#    - username (String 196)
+#    - platform (String 128)
+#    - status (String 128)
+#    - last_activity_at (Integer) (epoch timestamp)
+#
+#    See https://github.com/jeremyevans/sequel/blob/master/doc/migration.rdoc for more information on migrations.
+Sequel.migration do
+  change do
+    create_table(:players) do
+      primary_key :id
+      String :key, size: 128, null: false, unique: true
+      String :username, size: 196, null: false, unique: true
+      String :platform, size: 128, null: false
+      String :status, size: 128, null: false
+      Integer :last_activity_at, default: Sequel.function(:to_timestamp, Sequel.function(:now)), null: false
+    end
+  end
+end
